ace8d6b30467b95e6f0ae97cc3f921a063f4ed92
Dev0Dyssey <tmuk@protonmail.com>
2019-02-23 19:27:06 +0000

    Work on CSS styling of the <MealDetail /> component

--- src/Components/MealDetail.js
+++ src/Components/MealDetail.js
@@ -3,11 +3,11 @@ import React from 'react';
 const MealDetail = ({detail, mealName, showComponent, urls, nutrition}) => {
 
     return(
-        <div className="container" style={{display: `${showComponent}`}}>
+        <div className="meal-detail container" style={{display: `${showComponent}`}}>
             <img className="meal-photo"src={urls.regular}/>
                 <div className="meal-name">
                     {mealName}
-                    <div>
+                    <div className="information">
                         <h4>General Nutritional Information</h4>
                         Calories: {nutrition}
                     </div>
