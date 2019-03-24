6f96af8ac52f41ba07a072e9c926e4c340ed0aef
Dev0Dyssey <tmuk@protonmail.com>
2019-03-24 15:55:11 +0000

    Imported Redux-Thunk and created Async-Await API action creators to handle API calls. Next step is defining the redux store and wiring into the app

--- src/Components/Actions/index.js
+++ src/Components/Actions/index.js
@@ -1,3 +1,20 @@
+import unsplash from "../../api/unsplash";
+import mealdatabase from "../../api/mealdatabase";
+
+export const photoList = (value) => async dispatch => {
+        const response = await unsplash.get('/search/photos', {
+            params: {query: value}
+        });
+
+        dispatch({type: 'IMAGE_LIST', payload: response})
+    };
+
+export const nutritionInfo = (term) => async dispatch => {
+        const response = await mealdatabase.get(`${term}&app_id=d0c3cf36&app_key=11cf8167af78fde44896a9793debed00`);
+
+        dispatch({type: 'NUTRITION_INFO', payload: response})
+    };
+
 export const captureMeal = (meal) => {
     return {
         type: 'MEAL_CAPTURE',
