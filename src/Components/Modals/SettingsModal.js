a29a02b8f67402cecefb8b3094b8e3f60382db5b
Dev0Dyssey <tmuk@protonmail.com>
2019-03-31 21:04:07 +0100

    Styled the <SettingsModal /> component based on prototyped design. Included Google material design icons within settings modal window

--- src/Components/Modals/SettingsModal.js
+++ src/Components/Modals/SettingsModal.js
@@ -6,15 +6,17 @@ const SettingsModal = () => {
         <Modal 
         size={'tiny'} 
         dimmer={'blurring'} 
-        style={{height: '700px', backgroundColor: '#2ecc71'}} 
+        style={{height: '450px', width: '250px', borderRadius: '15px', backgroundColor: '#2ecc71'}} 
         trigger={<span>Settings</span>}
         >
 
-            <Modal.Header style={{backgroundColor: '#2ecc71'}}>Settings</Modal.Header>
-                <Modal.Content style={{backgroundColor: '#2ecc71'}}>
+            
+                <Modal.Content style={{backgroundColor: '#2ecc71', borderRadius: '15px', color: 'white'}}>
                     <Modal.Description>
-                        <Header>Settings</Header>
-                        <p>Change profile settings</p>
+                        <p style={{paddingBottom: '67px', fontSize: '17px'}}>Change Username<i className="material-icons" style={{marginLeft: '20px'}}>edit</i></p>
+                        <p style={{paddingBottom: '67px', fontSize: '17px'}}>Change Password<i class="material-icons" style={{marginLeft: '20px'}}>lock</i></p>
+                        <p style={{paddingBottom: '67px', fontSize: '17px'}}>Delete Account<i class="material-icons" style={{marginLeft: '20px'}}>cancel</i></p>
+                        <p style={{fontSize: '17px'}}>Privacy Policy<i class="material-icons" style={{marginLeft: '20px'}}>verified_user</i></p>
                     </Modal.Description>
                 </Modal.Content>
         </Modal>
