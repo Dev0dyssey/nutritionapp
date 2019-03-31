7a009a5a0136fae214a594492b2fdcec745edc0a
Dev0Dyssey <tmuk@protonmail.com>
2019-03-31 19:13:52 +0100

    Removed commented out code

--- src/Components/navbuttons.js
+++ src/Components/navbuttons.js
@@ -41,20 +41,7 @@ const NavButtons = () => {
                 <div className="item" style={{color: 'white'}}>
                     <i className="exclamation triangle icon"></i>
                     Report an Issue
-                </div>
-
-{/* <Modal trigger={<Button>Show Modal</Button>}>
-    <Modal.Header>Select a Photo</Modal.Header>
-    <Modal.Content image>
-      <Image wrapped size='medium' src='/images/avatar/large/rachel.png' />
-      <Modal.Description>
-        <Header>Default Profile Image</Header>
-        <p>We've found the following gravatar image associated with your e-mail address.</p>
-        <p>Is it okay to use this photo?</p>
-      </Modal.Description>
-    </Modal.Content>
-  </Modal> */}
-                
+                </div>                
             </div>
         </button>
     )
