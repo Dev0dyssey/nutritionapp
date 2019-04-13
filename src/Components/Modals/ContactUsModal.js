a397cd30a742b5be76e346605bb440d20767b559
Dev0Dyssey <tmuk@protonmail.com>
2019-04-13 21:12:09 +0100

    Added elements to the <ContactUsModal /> component and updated styling to be inline with the rest of the app. Next step is to import React Socks for responsive design.

--- src/Components/Modals/ContactUsModal.js
+++ src/Components/Modals/ContactUsModal.js
@@ -1,19 +1,34 @@
 import React from 'react';
-import { Modal } from 'semantic-ui-react';
+import { Modal, Checkbox, Input, Button } from 'semantic-ui-react';
 
 const ContactUsModal = () => {
     return (
         <Modal
             dimmer={'blurring'}
             trigger={<span>Contact Us</span>}
+            style={{width: '25%'}}
         >
             <Modal.Content style = {{backgroundColor: '#2ecc71', borderRadious: '15px', color: 'white'}}>
-                <Modal.Description>
-                    <form>
-                        <input placeholder="Reason of contact"></input>
-                            <br />
-                        <span>How do you wish us to contact you: </span>
-                    </form>
+                <Modal.Description style={{color: 'white'}}>
+                    <Input style={{width: '100%', marginBottom: '1em'}} placeholder="Reason of contact" />
+                        <br />
+                    <span style={{marginBottom: '1em', display: 'inline-block'}}>How do you wish us to contact you: </span>
+                        <br />
+
+                    <Checkbox value="email" /> Email
+                        <br/>
+                    <Checkbox value="chat"/> Chat
+                        <br />
+                    <Checkbox value="phone" /> Phone
+                        <br />
+
+                    <Button
+                        basic inverted
+                        size='small'
+                        style={{marginTop: '1em'}}
+                    >
+                        Submit
+                    </Button>
                 </Modal.Description>
             </Modal.Content>
         </Modal>
