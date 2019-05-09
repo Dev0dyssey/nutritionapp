8ad24d2d1fa16d26d6f8b40fd170d42158e550b5
ThomasMartinUK <tmuk@protonmail.com>
2019-05-09 06:31:12 +0100

    Began to coding the content of <ProfileModal /> to provide more functionality to the app user

--- src/Components/Modals/ProfileModal.js
+++ src/Components/Modals/ProfileModal.js
@@ -6,9 +6,24 @@ const ProfileModal = () => {
         <Modal
             size={'large'}
             dimmer={'blurring'}
-            style={{height: '450px', width: '450px', borderRadius: '15px', backgroundColor: '#2ecc71'}}
+            style={{height: '50%', width: '50%', borderRadius: '15px', backgroundColor: '#2ecc71'}}
             trigger={<span>Profile</span>}
-        ></Modal>
+        >
+
+            <Modal.Content style = {{backgroundColor: '#2ecc71', color: 'white', borderRadius: '50%'}}>
+                <Modal.Description>
+                    <div style={{textAlign: 'center'}}>
+                    <img
+                        style = {{borderRadius: '50%', height: '10rem', width: '10rem'}}
+                        alt = 'Profile'
+                        src = "https://bit.ly/2JEd0vj">
+                    </img>
+                    <h3>CHANGE PROFILE PICTURE</h3>
+                    </div>
+                </Modal.Description>
+            </Modal.Content>
+
+        </Modal>
     )
 }
 
