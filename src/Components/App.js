07dea91496f858a7dd45e9ab1add10fd9a2eef58
ThomasMartinUK <tmuk@protonmail.com>
2019-05-14 06:36:02 +0100

    Started placing App components into their respective Routes for navigation of the App

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 import MealCapture from './MealCapture';
+import TestRoute from './Routes/TestRoute';
 
 
 const App = () => {
     return(
         <div className="ui container">
-            <MealCapture/>
+            <BrowserRouter>
+                {/* <MealCapture/> */}
+                <Route path="/" exact component = { MealCapture } />
+                <Route path="/routes/test" exact component = { TestRoute } />
+            </BrowserRouter>
         </div>
     );
 }
