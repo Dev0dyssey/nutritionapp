d15b1dad84772e379bcfc0370f0a07a141ad5b92
ThomasMartinUK <tmuk@protonmail.com>
2019-06-02 21:00:03 +0100

    Start of adding Authentication to the application. Authentication component scaffolding done; ready to expand code

---
+++ src/Components/GoogleAuth.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+class GoogleAuth extends React.Component {
+
+    componentDidMount() {
+        window.gapi.load('client:auth2', () => {
+            window.gapi.client.init({
+                clientId: '917601408536-oj07f96vg3g7obm818dbvh5f1kikqkra.apps.googleusercontent.com',
+                scope: 'email'
+            }).then(()=> {
+                this.auth = window.gapi.auth2.getAuthInstance();
+                console.log(this.auth)
+            });
+        });
+    }
+
+    render(){
+        return(<div></div>);
+    }
+
+}
+
+export default GoogleAuth;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
+import { createStore } from 'redux';
 
 import App from './Components/App';
 import reducers from './Reducers'
