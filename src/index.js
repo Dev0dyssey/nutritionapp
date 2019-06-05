11cb99cb1f2b15b629e65cbc8bafb93de1ec9418
ThomasMartinUK <tmuk@protonmail.com>
2019-06-05 06:35:51 +0100

    Added Redux Dev tools to the App to allow easit deugging and monitoring of the App

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 
 import App from './Components/App';
 import reducers from './Reducers'
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+    reducers,
+    composeEnhancers(applyMiddleware())
+);
+
 ReactDOM.render(
-    <Provider store={createStore(reducers)}>
+    <Provider store={store}>
         <App />
     </Provider>, 
     document.querySelector('#root')
